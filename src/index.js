20720ba3c1f1b29f79b3ef031defafb89f00e229
Sadashiv Kale <sadashivkale604@gmail.com>
2025-07-16 18:50:30 +0530

    image upload setup done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const userRouter = require('./routes/userRoute.js');
 const cartRouter = require('./routes/cartRouter.js');
 const authRouter = require('./routes/authRoute.js');
 const { isLoggedIn } = require('./validation/authValidator.js');
+const uploader = require('./middlewares/multerMiddleware.js');
+const cloudinary = require('./config/cloudinaryConfig.js');
+const fs = require('fs/promises');
 
 // const User = require('./schema/userSchema.js');
 
@@ -27,7 +30,17 @@ app.get('/ping', isLoggedIn, (req, res) => {
     console.log(req.body);
     console.log(req.cookies)
     return res.json({message: "pong"})
-})
+});
+
+app.post('/photo', uploader.single('incommingFile'), async (req, res) => {
+    console.log(req.file);
+    const result = await cloudinary.uploader.upload(req.file.path);
+    console.log("result from cloudinary", result);
+    await fs.unlink(req.file.path);
+    return res.json({
+        message: 'ok'
+    })
+});
 
 app.listen(ServerConfig.PORT, async () => {
     await connectDB();
