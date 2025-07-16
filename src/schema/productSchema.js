a9f18f54b293c35422c8cd1f56135db30003a31f
Sadashiv Kale <sadashivkale604@gmail.com>
2025-07-16 18:00:14 +0530

    added cloudinary config

---
+++ src/schema/productSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { applyTimestamps } = require('./userSchema');
+
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: [true, "Product name is required"],
+        minLength: [5, "Product name mush be atleast 5 characters"],
+        trim: true
+    },
+    description: {
+        type: String,
+        minLength: [5, "Product description mush be atleast 5 characters"]
+    },
+    productImage: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"]
+    },
+    category: {
+        type: String,
+        enum: ['veg', 'non-veg', 'drinks', 'sides'],
+        default: 'veg'
+    },
+    inStock: {
+        type: Boolean,
+        required: [true, "In stosk status is required"],
+        default: true
+    },
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product
